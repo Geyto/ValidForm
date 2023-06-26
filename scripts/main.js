9ea7e24fbb8c07275a0d3855c9940687038ebb54
Roman Geyto <vrgeyto@gmail.com>
2023-06-26 19:06:03 +0300

    fix prevent default

--- scripts/main.js
+++ scripts/main.js
@@ -23,80 +23,101 @@ $(document).ready(function () {
         checkLabel: $('#label4'),
     };
     const onRegisterSubmit = (event) => {
+        event.preventDefault();
         authorizationInput.css('border-bottom', '2px solid #C6C6C4');
         $('.formError').hide();
         if (!formFields.name.val()) {
             formFields.name.css('border-bottom', '2px solid red');
             $('.nameHasError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.name.val().match(/^[А-ЯЁ][а-яё]+\s*$/)) {
             formFields.name.css('border-bottom', '2px solid red');
             $('.nameRegError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.username.val()) {
             formFields.username.css('border-bottom', '2px solid red');
             $('.fullNameHasError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.username.val().match(/^([а-яёА-ЯЁ0-9\-_]+$)|^([A-Za-z0-9\-_]+$)/)) {
             formFields.username.css('border-bottom', '2px solid red');
             $('.fullNameRegError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.mail.val()) {
             formFields.mail.css('border-bottom', '2px solid red');
             $('.mailHasError').show();
-            event.preventDefault();
             return
         }//Проверка через рег выражения работает, но я оставил проверку из атрибута тип email
         if (!formFields.mail.val().match(/^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-ЯЁа-яё]{1}[-0-9А-я\.]{1,}[0-9А-ЯЁа-яё]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u)) {
             formFields.mail.css('border-bottom', '2px solid red');
             $('.mailRegError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.passwordOne.val()) {
             formFields.passwordOne.css('border-bottom', '2px solid red');
             $('.passwordHasError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.passwordOne.val().match(/((?=.*[0-9])(?=.*[!@#$%^&*\-_])(?=.*[a-z])(?=.*[A-Z])([0-9a-zA-Z!\-_@#$%^&*]{8,}))|((?=.*[0-9])(?=.*[!@#$%^&*\-_])(?=.*[а-яё])(?=.*[А-ЯЁ])([0-9а-яёА-ЯЁ!\-_@#$%^&*]{8,}))/g)) {
             formFields.passwordOne.css('border-bottom', '2px solid red');
             $('.passwordRegError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.passwordTwo.val()) {
             formFields.passwordTwo.css('border-bottom', '2px solid red');
             $('.passwordRepeatHasError').show();
-            event.preventDefault();
             return
         }
         if (formFields.passwordOne.val() !== formFields.passwordTwo.val()) {
             formFields.passwordTwo.css('border-bottom', '2px solid red');
             $('.passwordRepeatNotEqualError').show();
-            event.preventDefault();
             return
         }
         if (!formFields.check.is(':checked')) {
             $('.checkError').show();
-            event.preventDefault();
             return
         } else {
             items.popup.addClass('popup__active');
-            event.preventDefault();
+
         }
         regForm.off('submit', onRegisterSubmit);
         regForm.on('submit', onLoginSubmit);
         formFields.check.prop('checked', false);
         saveClientInLocalStorage();
+    }
+    const onLoginSubmit = (event) => {
+        event.preventDefault();
+        authorizationInput.css('border-bottom', '2px solid #C6C6C4');
+        $('.formError').hide();
+        if (!formFields.username.val()) {
+            formFields.username.css('border-bottom', '2px solid red');
+            $('.fullNameHasError').show();
+            return
+        } else if (!formFields.passwordOne.val()) {
+            formFields.passwordOne.css('border-bottom', '2px solid red');
+            $('.passwordHasError').show();
+            return
+        } else if (formFields.passwordOne.val().length < 8) {
+            formFields.passwordOne.css('border-bottom', '2px solid red');
+            $('.passwordNotRightError').show();
+            return
+        }
+        let clients = localStorage.getItem("clients");
+        let clientsArray = JSON.parse(clients);
+        let clientObject = clientsArray.find(item => item.name === formFields.username.val());
+        if (!clientObject) {
+            $('.fullNameAuthorizationError').show();
+            return
+        }
+        if (clientObject.password !== formFields.passwordOne.val()) {
+            $('.passwordNotRightError').show();
+            return
+        }
+        regForm.off('submit', onLoginSubmit);
+        creationCabinetClients(clientObject);
     }
     const saveClientInLocalStorage = () => {
         let client = {};
@@ -112,41 +133,6 @@ $(document).ready(function () {
         }
         clientsArray.push(client);
         localStorage.setItem('clients', JSON.stringify(clientsArray));
-    }
-    const onLoginSubmit = (event) => {
-        authorizationInput.css('border-bottom', '2px solid #C6C6C4');
-        $('.formError').hide();
-        if (!formFields.username.val()) {
-            formFields.username.css('border-bottom', '2px solid red');
-            $('.fullNameHasError').show();
-            event.preventDefault();
-            return
-        } else if (!formFields.passwordOne.val()) {
-            formFields.passwordOne.css('border-bottom', '2px solid red');
-            $('.passwordHasError').show();
-            event.preventDefault();
-            return
-        } else if (formFields.passwordOne.val().length < 8) {
-            formFields.passwordOne.css('border-bottom', '2px solid red');
-            $('.passwordNotRightError').show();
-            event.preventDefault();
-            return
-        }
-        let clients = localStorage.getItem("clients");
-        let clientsArray = JSON.parse(clients);
-        let clientObject = clientsArray.find(item => item.name === formFields.username.val());
-        if (!clientObject) {
-            $('.fullNameAuthorizationError').show();
-            event.preventDefault();
-            return
-        }
-        if (clientObject.password !== formFields.passwordOne.val()) {
-            $('.passwordNotRightError').show();
-            event.preventDefault();
-            return
-        }
-        regForm.off('submit', onLoginSubmit);
-        creationCabinetClients(clientObject);
     }
     const creationCabinetClients = (event) => {
         items.title.text("Welcome, " + event.fullName);
